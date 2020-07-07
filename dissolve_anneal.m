8470f620fd2604c9a3a14e68efd0324ec23adf42
Andreas Theodorou <theoda@ipta.demokritos.gr>
2020-07-07 13:27:14 +0300

    Phase dissolution

---
+++ dissolve_anneal.m
@@ -0,0 +1,11 @@
+function [xdot2] = dissolve_anneal(x2,u,P,N1,k)
+
+xdot2 = zeros(size(x2));
+F = P.F0(k) .* x2(1,:).^3 .* N1(13);
+%Xc = (P.Xc0 - P.Xp.*F) ./ (1 - F);
+A.d = exp(1/P.Xp./x2(1,:));
+
+xdot2(1,:) = (P.a^2/P.R0s(k)^2./x2(1,:)) .*(x2(2,:) - P.Xeqs(k)*A.d) ./ (P.Xp - P.Xeqs(k)*A.d) ;
+
+xdot2(2,:) = (x2(2,:) - P.Xp) .* F ./ (1-F) .* 3.*xdot2(1,:)./x2(1,:);
+endfunction
