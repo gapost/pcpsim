d831199349e97a6a40a89ef6d8cfc790fe1a3b70
George Apostolopoulos <gapost@ipta.demokritos.gr>
2020-12-17 14:32:29 +0200

    Corrected mean_radius_ng.m - mean R equations.
    Previous coding did not allow dR/dt<0 during nucleation
    which was not right.
    No dR/dt<0 is always possible as long as R>0. If R<0 this means
    all particles dissolved and dR/dt=dX/dt=0

--- mean_radius/mean_radius_ng.m
+++ mean_radius/mean_radius_ng.m
@@ -8,9 +8,9 @@ function [xdot, F, S] = mean_radius_ng(t,x,Xp,Xeq,b0,dG0,R0,incub,dbg)
 % Input:
 %  t       : time (D*t/rat^2) 
 %  x(1,3)  : ODE variables, 
-%            x(1): precipitate atomic concentration, 
-%            x(2): mean radius (in units of rat), 
-%            x(3): atomic concentration of solute in matrix
+%            x(1): precipitate atomic concentration N, 
+%            x(2): mean radius R (in units of rat), 
+%            x(3): atomic concentration of solute in matrix X
 %  Xp, Xeq : solute conc. in the precipitate and in the matrix at equilibrium
 %  R0,dG0,b0 : nucleation & growth physical parameters
 %  incub   : if 1 then incubation time is calculated
@@ -22,7 +22,7 @@ function [xdot, F, S] = mean_radius_ng(t,x,Xp,Xeq,b0,dG0,R0,incub,dbg)
 %   S      : nucleation entropy
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-
+  
   N = x(1); R = x(2); X = x(3);
   if N<0, 
     error(['Negative N value, N=' num2str(N)]);
@@ -30,51 +30,46 @@ function [xdot, F, S] = mean_radius_ng(t,x,Xp,Xeq,b0,dG0,R0,incub,dbg)
   if X<0,
     error(['Negative X value, X=' num2str(X)]);
   endif
-
-  xdot = zeros(size(x));
-  y=0;
-
-  S = Xp*log(X/Xeq) +(1-Xp).*log((1-X)/(1-Xeq));
-  F = R^3 * N;
-  Xr = Xeq*exp(R0./R*(1-Xeq)/(Xp-Xeq));
-  Xr=min(0.9*Xp,Xr);
   
-  if S>0, % nucleation & growth
-    if R<0,
-      error(['Negative R in nucleation, R=' num2str(R)]);
-    endif
+  xdot = zeros(size(x));
+  y=0; % (1/N)dN/dt = xdot(1)/x(1)
+  
+  S = Xp*log(X/Xeq) +(1-Xp).*log((1-X)/(1-Xeq)); % nucleation entropy
+  F = R^3 * N; % precipitate volume fraction
+  
+  % solute conc. at prec./matrix interface
+  Xr = Xeq*exp(R0./R*(1-Xeq)/(Xp-Xeq));
+  Xr = min(0.9*Xp,Xr); % cannot go above Xp
+  
+  if S>0, % nucleation 
     S2 = S^2;
     b = X * b0 / S2;
-    xdot(1) = b .*exp(-dG0./S2);
-    if incub,
+    xdot(1) = b .*exp(-dG0./S2); % nucleation rate, no incubation
+    if incub, % with incubation
       if t>0,
         xdot(1) = xdot(1) .*exp(-1./2/b/t);
-        if N>1e-23,
+        if N>1e-23, % only if appreciable N > 1 per cm^3
           y = xdot(1,:)/N;
         else
           y = 0;
         endif
-      else
+      else % for t=0, nucl rate = 0
         xdot(1) = 0;
       end
-    else
+    else % no incubation
       if N>0,
         y=xdot(1)/N;
       endif
     endif
+  end
+  
+  % calc dR/dt & dX/dt only for R>0
+  % R<0 means precipitates have dissolved 
+  if R>0, 
+    xdot(2) = (X-Xr) / (Xp-Xr) / R;
+    xdot(3) = (X - Xp) * F / (1-F) * 3 * xdot(2) / R;
+  endif 
+  
+  if dbg, disp(num2str([t x' xdot'])); end
     
-    xdot(2) = (X-Xr) / (Xp-Xr) / R + y*(1.05*R0./S - R);
-    if xdot(2)<0, xdot(2)=0; end
-    xdot(3) = (X - Xp) * F / (1-F) * (3*xdot(2)/R + y );    
-  else % dissolution
-    if R>0, 
-      
-      xdot(2) = (X-Xr) / (Xp-Xr) / R;
-      xdot(3) = (X - Xp) * F / (1-F) * 3 * xdot(2) / R;
-    endif
-  endif
-
-
-  if dbg, disp(num2str([t x' xdot' y])); end
-
 endfunction 
