f4e167539a6211dbe4b9a8e37fa18fd664e535b3
George Apostolopoulos <gapost@ipta.demokritos.gr>
2021-06-24 13:06:17 +0300

    FeN nucleation example

---
+++ FeN/FeN_model.m
@@ -0,0 +1,76 @@
+function [x,F] = FeN_model(t,Ta,X0,gam,incub,solver)
+  
+% add path to pcpsim functions
+addpath("../m");
+
+% Constants
+kb = 8.617e-5; %boltzmann constant [eV/K]
+ 
+% Fe lattice data
+afe = 0.286e-9; % lattice parameter [m]
+
+% N diffusion in Fe
+D0 = 1.26e-7; %Pre-exp. diffusion [m^2/s]
+Qd = 0.76; % migration energy for diffusion [eV]
+D =D0*exp(-Qd./(kb*Ta)); %diffusion coefficient [m^2/s]
+
+% alloy data
+Xeq = 10.^(2.43 - 1840./Ta) *1e-2; % N conc. in equil. with a" from ???
+Xp = 1/9; % precipitate N conc.
+
+% get calculation parameters
+[rat,tau,b0,dG0,R0] = ngparam(afe,2,gam,D,Ta);
+
+
+nt = length(t);
+nTa = length(Ta);
+
+if nTa==1, % isothermal
+    % initial condition
+    S = Xp*log(X0/Xeq) +(1-Xp).*log((1-X0)/(1-Xeq));
+    x0 = [0; 1.05*R0/S; X0];
+
+    % call dae solver
+    x = ngdae(t/tau,x0,...
+      Xp,Xeq,b0,dG0,R0,incub,solver);
+
+else % nTa>1 -> isochronal
+  if nt>1 && nt~=nTa,
+    error('For isochronal give a single time or a t(1:n) where n=length(Ta)')
+  end
+  if nt==1,
+    t = ones(1,nTa)*t;
+  end
+
+  x = zeros(nTa,3);
+    
+      
+    for i=1:nTa
+  
+      if i==1,
+        s = Xp*log(X0./Xeq(1))+(1-Xp)*log((1-X0)./(1-Xeq(1)));
+        y = [0 1.05*R0(1)/s X0]';    
+      else
+        y = x(i-1,:)';
+        
+        % check the following:
+        %   1) we have nucleation (dN/dt>0) 
+        %   2) current R is below the new Rc   
+        % if yes, delete all nuclei (they are unstable)
+        s = Xp*log(y(3)./Xeq(i))+(1-Xp)*log((1-y(3))./(1-Xeq(i)));
+        jn = y(3)*b0(i)/s^2*exp(-dG0(i)/s^2);
+        if y(2) < 1.05*R0(i)/s && jn>0,
+          y = [0 1.05*R0(i)/s X0]'; 
+        end
+      end
+
+        y = ngdae(linspace(0,t(i)/tau(i),21),y,...
+           Xp,Xeq(i),b0(i),dG0(i),R0(i),incub,solver);
+      
+      x(i,:) = y(end,:);
+      
+      
+    end
+  end
+
+  F = x(:,1).*x(:,2).^3;
---
+++ FeN/FeN_nucl.m
@@ -0,0 +1,49 @@
+% Calculate Fe - 880 ppm N nucleation & growth at 373 K
+% Exp: Abiko & Imai 1977
+% model by mean R equations
+clear
+
+
+% annealing conditions
+Ta = 373; % ageing T (K)
+dt = 1e6; % ageing time in s
+
+% Options
+incub=1; % Calc. incubation time for nucleation
+
+% alloy data
+gam = 0.062; % surface tension [J/m^2]
+X0 = 8.8e-4; % Initial N concentration
+
+
+% log time grid - 10 pts per decade
+% t in s
+nt = (log10(dt)+1)*10 + 1;
+t = logspace(-1,log10(dt),nt);
+
+solver = 'daspk'; % 'ode15i', 'daspk' (octave only)
+[x,F] = FeN_model(t,Ta,X0,gam,incub,solver);
+
+clf
+subplot(2,2,1)
+semilogx(t,x(:,1),'.-')
+title('Clusters per atom');
+
+subplot(2,2,2)
+loglog(t,x(:,2),'.-')
+ylabel('nm');
+title('radius R')
+
+subplot(2,2,3)
+loglog(t,x(:,3),'.-')
+title('matrix X_N');
+xlabel('t (s)');
+
+subplot(2,2,4)
+semilogx(t,F,'.-')
+title('Transformed volume fraction ');
+xlabel('t (s)');
+
+print -dpdfcrop FeN_nucl
+
+
